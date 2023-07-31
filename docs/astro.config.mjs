0bf8bc627ddde89787ada35e8226f6368620eafa
Ernest Badu <ernestjbadu@outlook.com>
2023-07-31 12:06:00 +0100

    feat: improve docs styling

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,16 +1,34 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import sitemap from '@astrojs/sitemap';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import remarkSmartypants from 'remark-smartypants';
+import { autolinkConfig } from './plugins/rehype-autolink-config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pulsarjs.dev',
+	markdown: {
+		remarkPlugins: [[remarkSmartypants, { dashes: false }]],
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+		],
+	},
 	integrations: [
 		sitemap(),
 		starlight({
 			title: 'Pulsar',
 			description:
 				'A dead-simple, type-safe web framework built on Web standards for the edge.',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/app.postcss',
+				'@fontsource/inter/400.css',
+				'@fontsource/inter/600.css',
+			],
 			defaultLocale: 'root', // optional
 			locales: {
 				root: {
@@ -29,10 +47,11 @@ export default defineConfig({
 				},
 			},
 			editLink: {
-				baseUrl: 'https://github.com/Ernxst/pulsar/edit/main/',
+				baseUrl: 'https://github.com/Ernxst/pulsar/edit/main/docs',
 			},
 			social: {
 				github: 'https://github.com/Ernxst/pulsar',
+				discord: 'https://discord.gg/',
 			},
 			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 3 },
 			lastUpdated: true,
