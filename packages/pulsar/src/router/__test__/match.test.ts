d97ae95ffffc4c6c10baa0b16a7745efb2c9ca50
Ernest Badu <ernestjbadu@outlook.com>
2023-08-01 20:49:15 +0100

    chore: add more test scenarios

--- packages/pulsar/src/router/__test__/match.test.ts
+++ packages/pulsar/src/router/__test__/match.test.ts
@@ -1,4 +1,5 @@
 import { beforeEach, describe, expect, test, vi } from 'vitest';
+import type { HttpMethod } from 'src';
 import { Pulsar } from '..';
 
 function request(method: string, url: string) {
@@ -8,7 +9,14 @@ function request(method: string, url: string) {
 }
 
 describe('When matching routes', () => {
-	describe.each([{ method: 'GET' }] as const)(
+	describe.each([
+		{ method: 'GET' },
+		{ method: 'POST' },
+		{ method: 'PUT' },
+		{ method: 'PATCH' },
+		{ method: 'DELETE' },
+		{ method: 'OPTIONS' },
+	] satisfies { method: HttpMethod }[])(
 		'given a $method route',
 		({ method }) => {
 			let router: Pulsar;
@@ -17,60 +25,51 @@ describe('When matching routes', () => {
 				router = new Pulsar();
 			});
 
-			describe.each([{ path: '/healthcheck' }] as const)(
-				'given a $path path',
-				({ path }) => {
-					const handler = vi.fn().mockResolvedValue({ message: 'hello' });
+			describe.each([
+				{ path: '/healthcheck', match: '/healthcheck' },
+				{ path: '/oauth/callback', match: '/oauth/callback' },
+				{ path: '/:id', match: '/123' },
+				{ path: '/:id/:name', match: '/123/abc' },
+				{ path: '/:id/:name?', match: '/123' },
+				{ path: '/:id/:name?', match: '/123/abc' },
+				{ path: '/users/:id', match: '/users/123' },
+				{ path: '/users/:id/:name', match: '/users/123/abc' },
+				{ path: '/users/:id/:name?', match: '/users/123' },
+				{ path: '/users/:id/:name?', match: '/users/123/abc' },
+			] as const)('given a $path path', ({ path, match }) => {
+				const handler = vi.fn().mockResolvedValue({ message: 'hello' });
 
-					beforeEach(() => {
-						const fn = method.toLowerCase() as Lowercase<typeof method>;
-						router[fn](path, handler);
-					});
+				beforeEach(() => {
+					const fn = method.toLowerCase() as Lowercase<typeof method>;
+					router[fn](path, handler);
+				});
 
-					describe('when the path matches', () => {
-						describe('when the method matches', () => {
-							let response: Response;
-
-							beforeEach(async () => {
-								const req = request(method, path);
-								response = await router.fetch(req);
-							});
-
-							test('should call route handler', () => {
-								expect(handler).toHaveBeenCalled();
-							});
-
-							test('should return 200', () => {
-								expect(response.ok).toBe(true);
-								expect(response.status).toBe(200);
-								expect(response.statusText).toBe('OK');
-							});
-						});
-
-						describe('when the method does not match', () => {
-							let response: Response;
-
-							beforeEach(async () => {
-								response = await router.fetch(request('UNKNOWN', path));
-							});
-
-							test('should not call route handler', () => {
-								expect(handler).not.toHaveBeenCalled();
-							});
-
-							test('should return 405', () => {
-								expect(response.ok).toBe(false);
-								expect(response.status).toBe(405);
-								expect(response.statusText).toBe('Method not allowed');
-							});
-						});
-					});
-
-					describe('when the path does not match', () => {
+				describe(`when ${match} matches`, () => {
+					describe('when the method matches', () => {
 						let response: Response;
 
 						beforeEach(async () => {
-							response = await router.fetch(request(method, '/unknown'));
+							const req = request(method, match);
+							response = await router.fetch(req);
+						});
+
+						test('should call route handler', () => {
+							expect(handler).toHaveBeenCalled();
+						});
+
+						test('should return 200', () => {
+							expect(response.ok).toBe(true);
+							expect(response.status).toBe(200);
+							expect(response.statusText).toBe('OK');
+						});
+					});
+
+					describe('when the method does not match', () => {
+						let response: Response;
+
+						beforeEach(async () => {
+							const unknown = method === 'GET' ? 'POST' : 'GET';
+							response = await router.fetch(request(unknown, match));
 						});
 
 						test('should not call route handler', () => {
@@ -83,8 +82,26 @@ describe('When matching routes', () => {
 							expect(response.statusText).toBe('Not found');
 						});
 					});
-				}
-			);
+				});
+
+				describe('when the path does not match', () => {
+					let response: Response;
+
+					beforeEach(async () => {
+						response = await router.fetch(request(method, ''));
+					});
+
+					test('should not call route handler', () => {
+						expect(handler).not.toHaveBeenCalled();
+					});
+
+					test('should return 404', () => {
+						expect(response.ok).toBe(false);
+						expect(response.status).toBe(404);
+						expect(response.statusText).toBe('Not found');
+					});
+				});
+			});
 		}
 	);
 });
