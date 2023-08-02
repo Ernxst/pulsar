a36d3b09ea4789f7d593edf169ba6dbc25e05a78
Ernest Badu <ernestjbadu@outlook.com>
2023-08-02 17:36:52 +0100

    chore: update tests

--- packages/pulsar/src/router/__test__/error.test.ts
+++ packages/pulsar/src/router/__test__/error.test.ts
@@ -1,21 +1,20 @@
 import { type Mock, beforeEach, describe, expect, test, vi } from 'vitest';
 import { z } from 'zod';
-import type { Path } from 'src';
+import { ValidationError } from 'src/errors';
 import { Pulsar } from '..';
 
 describe('Custom error handling', () => {
-	let router: Pulsar;
 	let errorHandler: Mock;
 
 	beforeEach(() => {
 		errorHandler = vi.fn(() => ({ error: true }));
-		router = new Pulsar().onError(errorHandler);
 	});
 
 	describe('when handling a 404 error', () => {
 		let response: Response;
 
 		beforeEach(async () => {
+			const router = new Pulsar().onError(errorHandler);
 			response = await router.fetch(new Request('https://example.com/'));
 		});
 
@@ -25,6 +24,7 @@ describe('Custom error handling', () => {
 		});
 
 		test('should call custom error handler', () => {
+			expect(errorHandler).toHaveBeenCalled();
 			expect(errorHandler).toHaveBeenCalledWith(
 				expect.objectContaining({
 					code: 'NOT_FOUND',
@@ -44,53 +44,93 @@ describe('Custom error handling', () => {
 		});
 	});
 
-	describe('when handling route errors', () => {
-		let path: Path = '/test';
-		let request = new Request('https://example.com/test');
-		let handler: Mock;
+	describe('when handling a route error', () => {
+		let routeHandler: Mock;
+		let response: Response;
 
-		beforeEach(() => {
-			handler = vi.fn(() => {});
-			router.get(path, handler);
+		beforeEach(async () => {
+			routeHandler = vi.fn().mockRejectedValue(new Error('test'));
+
+			const router = new Pulsar()
+				.onError(errorHandler)
+				.get('/test', routeHandler);
+
+			const request = new Request('https://example.com/test');
+			response = await router.fetch(request);
 		});
 
-		describe('when handling a validation error', () => {
-			path = '/users';
-			request = new Request('https://example.com/users', { method: 'POST' });
+		test('should call route handler', () => {
+			expect(routeHandler).toHaveBeenCalled();
+		});
 
-			let response: Response;
+		test('should call custom error handler', () => {
+			expect(errorHandler).toHaveBeenCalled();
+			expect(errorHandler).toHaveBeenCalledWith(
+				expect.objectContaining({
+					code: 'INTERNAL_SERVER_ERROR',
+					error: expect.any(Error),
+				})
+			);
+		});
 
-			beforeEach(async () => {
-				router.post(path, { body: { id: z.string() } }, handler);
-				response = await router.fetch(request);
-			});
+		test('should use response from error handler', async () => {
+			const data = await response.text();
+			expect(data).toEqual(JSON.stringify({ error: true }));
+		});
 
-			test('should call handler', () => {
-				expect(handler).toHaveBeenCalled();
-			});
+		// TODO: Need to set context using middleware for 404s
+		test.todo('should maintain context from middleware', () => {
+			expect(errorHandler).toHaveBeenCalled();
+			expect(errorHandler).toHaveBeenCalledWith(
+				expect.objectContaining({
+					path: new URL('https://example.com/'),
+				})
+			);
+		});
+	});
 
-			test('should use response from error handler', async () => {
-				const data = await response.text();
-				expect(data).toEqual(JSON.stringify({ error: true }));
-			});
+	describe('when handling a validation error', () => {
+		let response: Response;
+		let routeHandler: Mock;
 
-			test('should call custom error handler', () => {
-				expect(errorHandler).toHaveBeenCalledWith(
-					expect.objectContaining({
-						code: 'VALIDATION',
-					})
-				);
-			});
+		beforeEach(async () => {
+			routeHandler = vi.fn();
 
-			// TODO: Need to set context using middleware for 404s
-			test.todo('should maintain context from middleware', () => {
-				expect(errorHandler).toHaveBeenCalled();
-				expect(errorHandler).toHaveBeenCalledWith(
-					expect.objectContaining({
-						path: new URL('https://example.com/'),
-					})
-				);
-			});
+			const router = new Pulsar()
+				.onError(errorHandler)
+				.post('/test', { body: { id: z.string() } }, routeHandler);
+
+			const req = new Request('https://example.com/test', { method: 'POST' });
+			response = await router.fetch(req);
+		});
+
+		test('should not call route handler', () => {
+			expect(routeHandler).not.toHaveBeenCalled();
+		});
+
+		test('should call custom error handler', () => {
+			expect(errorHandler).toHaveBeenCalled();
+			expect(errorHandler).toHaveBeenCalledWith(
+				expect.objectContaining({
+					code: 'VALIDATION',
+					error: expect.any(ValidationError),
+				})
+			);
+		});
+
+		test('should use response from error handler', async () => {
+			const data = await response.text();
+			expect(data).toEqual(JSON.stringify({ error: true }));
+		});
+
+		// TODO: Need to set context using middleware for 404s
+		test.todo('should maintain context from middleware', () => {
+			expect(errorHandler).toHaveBeenCalled();
+			expect(errorHandler).toHaveBeenCalledWith(
+				expect.objectContaining({
+					path: new URL('https://example.com/'),
+				})
+			);
 		});
 	});
 });
